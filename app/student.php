0de3902fc53fc60919d7020ad479af0395f99639
YourUsername <you@example.com>
2025-07-23 09:05:31 +0545

    done Student.php

--- app/student.php
+++ app/student.php
@@ -6,15 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-   
-      protected $fillable = [
-        'name',
-        'email',
-        'faculty',
-        'lab_id',
-        'assignment_id',
-        // 'password' is NOT in your students table, so don't include it here
-    ];
+    //
 }
-
- 
